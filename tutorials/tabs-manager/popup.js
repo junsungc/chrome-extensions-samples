0dfb09191a79ea22b870516e934c11eacfbdac93
JunSung Choi <junsungc91@gmail.com>
2022-12-30 23:05:57 +0900

    tutorials/tabs-manager : 같은 hostname을 가진 경우 자동 그룹화 - 기능 추가

--- tutorials/tabs-manager/popup.js
+++ tutorials/tabs-manager/popup.js
@@ -12,40 +12,54 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-const tabs = await chrome.tabs.query({
-  url: [
-    "https://developer.chrome.com/docs/webstore/*",
-    "https://developer.chrome.com/docs/extensions/*",
-  ],
-});
+const tabs = await chrome.tabs.query({});
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/Collator
 const collator = new Intl.Collator();
-tabs.sort((a, b) => collator.compare(a.title, b.title));
+tabs.sort((a, b) => collator.compare(a.index, b.index));
 
 const template = document.getElementById("li_template");
 const elements = new Set();
 for (const tab of tabs) {
-  const element = template.content.firstElementChild.cloneNode(true);
+  if (tab.url) {
+    const element = template.content.firstElementChild.cloneNode(true);
+    const tabUrl = new URL(tab.url)
+    const title = tabUrl.hostname
+    const pathname = tabUrl.pathname
 
-  const title = tab.title.split("-")[0].trim();
-  const pathname = new URL(tab.url).pathname.slice("/docs".length);
+    element.querySelector(".title").textContent = title;
+    element.querySelector(".pathname").textContent = pathname;
+    element.querySelector("a").addEventListener("click", async () => {
+      // need to focus window as well as the active tab
+      await chrome.tabs.update(tab.id, { active: true });
+      await chrome.windows.update(tab.windowId, { focused: true });
+    });
 
-  element.querySelector(".title").textContent = title;
-  element.querySelector(".pathname").textContent = pathname;
-  element.querySelector("a").addEventListener("click", async () => {
-    // need to focus window as well as the active tab
-    await chrome.tabs.update(tab.id, { active: true });
-    await chrome.windows.update(tab.windowId, { focused: true });
-  });
-
-  elements.add(element);
+    elements.add(element);
+  }
 }
+
+const tabObj = tabs.reduce((acc, cur, idx) => {
+  if (cur.url) {
+    const tabUrl = new URL(cur.url).hostname
+    if (acc[tabUrl]) {
+      acc[tabUrl].push(cur.id)
+    } else {
+      acc[tabUrl] = [cur.id]
+    }
+  }
+  return acc;
+}, {})
 document.querySelector("ul").append(...elements);
 
 const button = document.querySelector("button");
 button.addEventListener("click", async () => {
-  const tabIds = tabs.map(({ id }) => id);
-  const group = await chrome.tabs.group({ tabIds });
-  await chrome.tabGroups.update(group, { title: "DOCS" });
+  const keys = Object.keys(tabObj)
+  keys.map(async key => {
+    if (tabObj[key].length > 1) {
+      const tabIds = tabObj[key]
+      const group = await chrome.tabs.group({ tabIds });
+      await chrome.tabGroups.update(group, { title: key });
+    }
+  })
 });
